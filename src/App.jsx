51029ad9cb6a7b296db8a19af3bdef96da12fdd4
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-02-29 15:27:46 +0530

    Sign In Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import SignIn from './components/SignIn/SignIn'
 import SignUp from './components/SignUp/SignUp'
 
 function App() {
   return (
     <div>
-      <SignUp />
+      <SignUp></SignUp>
     </div>
   )
 }
