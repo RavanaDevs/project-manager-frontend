b968b7f437c8a4f17b8bb73c85d4067ca117d37b
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-02 11:09:59 +0530

    New components

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+} from 'firebase/auth'
+import { auth } from '../firebase'
+
+const AuthContext = createContext()
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const logout = () => {
+    return signOut(auth)
+  }
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email)
+  }
+
+  const updateEmail = (email) => {
+    // return updateEmail(auth,email);
+    throw Error('Not implimented')
+  }
+
+  const updatePassword = (password) => {
+    // return currentUser.updatePassword(password);
+    throw Error('Not implimented')
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
