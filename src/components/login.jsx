795f9b9b32b56e7def68f017d4eb022b6bf1d190
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-02 22:25:23 +0530

    Change theme to dark

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,13 +1,13 @@
-import { useRef, useState } from 'react'
-import { Container, Form, Button, Card, Alert } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import { useRef, useState } from "react"
+import { Container, Form, Button, Card, Alert } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
 
 const Login = () => {
   const emailRef = useRef()
   const passwordRef = useRef()
   const { login } = useAuth()
-  const [error, setError] = useState('')
+  const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
@@ -15,12 +15,12 @@ const Login = () => {
     e.preventDefault()
 
     try {
-      setError('')
+      setError("")
       setLoading(true)
       await login(emailRef.current.value, passwordRef.current.value)
-      navigate('/')
+      navigate("/")
     } catch {
-      setError('Failed to log in')
+      setError("Failed to log in")
     }
 
     setLoading(false)
@@ -29,34 +29,53 @@ const Login = () => {
   return (
     <>
       <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: '100vh' }}
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: "100vh" }}
       >
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Card>
+        <div className='w-100' style={{ maxWidth: "400px" }}>
+          <Card className='bg-dark text-white'>
             <Card.Body>
-              <h2 className="text-center mb-4">Log In</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
+              <h2 className='text-center mb-4'>Log In</h2>
+              {error && <Alert variant='danger'>{error}</Alert>}
               <Form onSubmit={handleSubmit}>
-                <Form.Group id="email">
+                <Form.Group id='email'>
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" ref={emailRef} required />
+                  <Form.Control
+                    type='email'
+                    ref={emailRef}
+                    required
+                    className='bg-secondary text-white'
+                  />
                 </Form.Group>
-                <Form.Group id="password">
+                <Form.Group id='password' className='mt-3'>
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" ref={passwordRef} required />
+                  <Form.Control
+                    type='password'
+                    ref={passwordRef}
+                    required
+                    className='bg-secondary text-white'
+                  />
                 </Form.Group>
-                <Button disabled={loading} className="w-100 mt-4" type="submit">
+                <Button
+                  disabled={loading}
+                  className='w-100 mt-4 '
+                  type='submit'
+                >
                   Log In
                 </Button>
               </Form>
-              <div className="w-100 text-center mt-3">
-                <Link to="/forgot-password">Forgot Password?</Link>
+              <div className='w-100 text-center mt-3'>
+                <Link to='/forgot-password' className='text-info'>
+                  Forgot Password?
+                </Link>
               </div>
             </Card.Body>
           </Card>
-          <div className="w-100 text-center mt-2">
-            Need an account? <Link to="/signup">Sign Up</Link>
+          <div className='w-100 text-center mt-2 text-light'>
+            Need an account?
+            <Link to='/signup' className='text-info'>
+              Sign Up
+            </Link>
           </div>
         </div>
       </Container>
